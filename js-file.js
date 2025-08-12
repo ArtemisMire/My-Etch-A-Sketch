09ee3099584017bd00f6aed782eb37c647f997e2
Kenny7SP <goodmorningvo3@gmail.com>
2025-08-12 13:57:54 +0300

    Finish script file, logic of the site

---
+++ js-file.js
@@ -0,0 +1,87 @@
+const inner = document.querySelector('#inner');
+const girdSize = document.querySelector('#gridSize');
+inner.classList.toggle('inner');
+const div = document.createElement('div');
+let cubeSize = (600 / 16);
+div.style.width = (cubeSize + "px");
+div.style.height = (cubeSize + "px");
+for (i = 0; i < 16; i++) {
+    for (j = 0; j < 16; j++) {
+        div.classList.add('cell');
+        inner.appendChild(div.cloneNode(true));
+    };
+};
+
+girdSize.addEventListener('click', (e) => {
+    console.log(girdSize.value);
+    let size = girdSize.value;
+    let cubeSize = (600 / size);
+    div.style.width = (cubeSize + "px");
+    div.style.height = (cubeSize + "px");
+    inner.innerHTML = '';
+    for (i=0; i<size; i++) {
+        for(j=0; j<size; j++) {
+            div.classList.add('cell');
+            inner.appendChild(div.cloneNode(true));
+        };
+    };
+    cells = document.querySelectorAll('.cell');
+    coloredCells = [];
+    changeCellColor(cells);
+});
+
+
+let cells = document.querySelectorAll('.cell');
+const tools = document.querySelector('#tools');
+
+let currentTool = '';
+var currentColor = 'black';
+function getCurrentTool(tools) {
+    tools.addEventListener('click', event => {        
+        let target = event.target;
+        switch (target.id) {
+            case 'clearGrid':                
+                coloredCells.forEach(cell => cell.style.background = 'white');
+                break
+            case 'eraser':
+                currentTool = 'eraser';
+                break;
+            case 'rgb':
+                currentTool = 'rgb';    
+                break;
+            case 'pallete' :
+                currentTool = 'pallete';
+                break;
+        };
+    });
+}
+
+function getCurrentColor(currentTool) {
+    if (currentTool == 'eraser') currentColor = 'white';
+    else if (currentTool == 'pallete') {
+        currentColor = document.querySelector('#pallete').value;
+        console.log(currentColor, currentTool);
+    }
+    return currentColor;
+}
+function getRandomColor() {
+    let r = (Math.round(Math.random() * 255));
+    let g = (Math.round(Math.random() * 255));
+    let b = (Math.round(Math.random() * 255));
+    return (`${r}, ${g}, ${b}`);
+}
+
+let coloredCells = [];
+function changeCellColor(cells) {
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', () => {
+            getCurrentTool(tools);
+            if (currentTool != 'rgb') getCurrentColor(currentTool);
+            else if (currentTool == 'rgb') currentColor = `rgb(${getRandomColor()})`;
+            cell.style.background = currentColor;
+            
+            coloredCells.push(cell);
+        });
+    });
+}
+changeCellColor(cells);
